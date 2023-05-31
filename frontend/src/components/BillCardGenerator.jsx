a1bda246161f0c6f8a40b988577ee155f04cefb0
jibranabsarulislam <jga26@njit.edu>
2023-05-31 12:13:40 -0400

    add date and other impl

--- frontend/src/components/BillCardGenerator.jsx
+++ frontend/src/components/BillCardGenerator.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 import BillCard from './BillCard';
 
 function BillCardGenerator() {
   const [bills, setBills] = useState([{}]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch('/get_all_bills')
@@ -14,26 +16,47 @@ function BillCardGenerator() {
       .then((data) => setBills(data));
   }, []);
 
+  // search box to filter bills by name
+
   const billArray = [];
 
   let i = 0;
-  bills.forEach((bill) => {
-    billArray.push(
-      <BillCard
-        key={`card-${i += 1}`}
-        account_id={bill.account_id}
-        due_date={bill.due_date}
-        name={bill.name}
-        url={bill.url}
-        overdue={bill.overdue}
-      />,
-    );
+
+  useEffect(() => {
+    bills.forEach((bill) => {
+      billArray.push(
+        <BillCard
+          key={`card-${i += 1}`}
+          account_id={bill.account_id}
+          due_date={bill.due_date}
+          name={bill.name}
+          url={bill.url}
+          overdue={bill.overdue}
+        />,
+      );
+    });
   });
 
   return (
-    <Container id="gen-container">
-      <Row>{billArray}</Row>
-    </Container>
+    <>
+      <Container id="search-container">
+        <input
+          type="text"
+          placeholder="Search bills by name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={() => {
+          }}
+        />
+      </Container>
+      <Container id="gen-container">
+        <Row>{billArray}</Row>
+      </Container>
+    </>
   );
 }
 
--- frontend/src/components/Transactions.jsx
+++ frontend/src/components/Transactions.jsx
@@ -1,22 +1,119 @@
 import React, { useState, useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
+// import Table from 'react-bootstrap/Table';
+import { Table } from 'antd';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Transactions() {
   const [data, setData] = useState([{}]);
+  const [beginDate, setBeginDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [trigger, setTrigger] = useState(false);
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: 'amount',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'Time',
+      dataIndex: 'time',
+      key: 'time',
+    },
+    {
+      title: 'Notes',
+      dataIndex: 'notes',
+      key: 'notes',
+    },
+  ];
+  let dataSource;
+
+  const updateDataSource = (newData) => {
+    dataSource = newData.map((transaction) => ({
+      key: transaction._id,
+      name: transaction.name,
+      amount: transaction.amount,
+      date: transaction.date,
+      time: transaction.time,
+      notes: transaction.notes,
+    }));
+  };
+
   useEffect(() => {
     fetch('/get_all_transactions')
       .then((response) => response.json())
       .then((result) => {
         setData(result.transactions);
       });
-    // setTransactions(data);
-  }, []);
-  console.log(data);
+
+    updateDataSource(data);
+  }, [trigger]);
+
+  useEffect(() => {
+    fetch('/get_transactions_between_dates', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        beginDate,
+        endDate,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result.transactions);
+      });
+
+    updateDataSource(data);
+  }, [beginDate, endDate]);
 
   return (
     <>
+      <Form>
+
+        <Form.Label>Begin Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="beginDate"
+          onChange={(e) => setBeginDate(e.target.value)}
+        />
+
+        <Form.Label>End Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="endDate"
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+      </Form>
+      {/* button to clear dates, set beginDate and endDate to '' and fetch all transactions */}
+      <Button
+        variant="primary"
+        type="submit"
+        onClick={() => {
+          setBeginDate('');
+          setEndDate('');
+          setTrigger(!trigger);
+        }}
+      >
+        Clear
+
+      </Button>
+
       <h3>Transaction History</h3>
-      {data.length > 0 && (
+      {/* {data.length > 0 && (
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -40,7 +137,12 @@ function Transactions() {
           ))}
         </tbody>
       </Table>
+      )} */}
+
+      {data.length > 0 && (
+      <Table dataSource={dataSource} columns={columns} />
       )}
+
       <h4>
         Total amount paid:
         $
